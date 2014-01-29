20b929c0447a481455b6eeac4c12e8b66ed8b1ed
Tschokee <fre-mail@freemail.hu>
2014-01-29 17:42:04 +0100

    Initial version

---
+++ Konyvelo/Konyvelo/Bejegyzés.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konyvelo
+{
+    public class Bejegyzés
+    {
+        public int sorSzám { get; set; }
+        public string fizetésIdeje { get; set; }
+        public string megjegyzés { get; set; }
+        public KöltségvetésiCím főkönyv { get; set; }
+        public int bankiBevétel { get; set; }
+        public int bankiKiadás { get; set; }
+        public static int bankiEgyenleg = 0; 
+        public int pénztáriBevétel { get; set; }
+        public int pénztáriKiadás { get; set; }
+        public static int pénztáriEgyenleg = 0;
+
+        public Bejegyzés(int sorSzám, string fizetésIdeje, string megjegyzés, KöltségvetésiCím főkönyv, int bankiBevétel, int bankiKiadás, int pénztáriBevétel, int pénztáriKiadás)
+        {
+            this.sorSzám = sorSzám;
+            this.fizetésIdeje = fizetésIdeje;
+            this.megjegyzés = megjegyzés;
+            this.főkönyv = főkönyv;
+            this.bankiBevétel = bankiBevétel;
+            this.bankiKiadás = bankiKiadás;
+            this.pénztáriBevétel = pénztáriBevétel;
+            this.pénztáriKiadás = pénztáriKiadás;
+
+            bankiEgyenleg = bankiEgyenleg + bankiBevétel - bankiKiadás;
+            pénztáriEgyenleg = pénztáriEgyenleg + pénztáriBevétel - pénztáriKiadás;
+        }
+    }
+}
---
+++ Konyvelo/Konyvelo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Konyvelo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Bejegyzés> LoadCollectionData()
+        {
+            List<Bejegyzés> bejegyzések = new List<Bejegyzés>();
+            bejegyzések.Add(new Bejegyzés(1, "tegnap", "asdfashdl", new KöltségvetésiCím(), 1000, 300, 500, 600));
+            return bejegyzések;
+        } 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyDataGrid.ItemsSource = LoadCollectionData();
+        }
+    }
+}
