89f0e9cee9e5604c79384cff0e4a267b41d324f9
Tschokee <fre-mail@freemail.hu>
2014-02-01 13:09:21 +0100

    Delete function corrected
    No more index out of range exception

--- Konyvelo/Konyvelo/MainWindow.xaml.cs
+++ Konyvelo/Konyvelo/MainWindow.xaml.cs
@@ -54,7 +54,7 @@ namespace Konyvelo
         private void torolButton_Click(object sender, RoutedEventArgs e)
         {
             int t = Convert.ToInt32(MyDataGrid.SelectedIndex.ToString());
-            if(t < bejegyzések.Count()) bejegyzések.RemoveAt(t);
+            if(t < bejegyzések.Count() && t > -1) bejegyzések.RemoveAt(t);
 
             //global bank és pénztári egyenleg módosítása szükséges, lehetőleg függvény segítségével ami újraszámolja a törölt elem előttitől az összeset
 
