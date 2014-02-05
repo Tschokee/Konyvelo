b7055bdf2197f62264a5ba659cdf690003333957
Alterion <hevnerjaons@hotmail.com>
2014-02-05 13:56:01 +0100

    Major update in koltsegvetesWindow

--- Konyvelo/Konyvelo/KoltsegvetesWindow.xaml.cs
+++ Konyvelo/Konyvelo/KoltsegvetesWindow.xaml.cs
@@ -19,6 +19,22 @@ namespace Konyvelo
     /// </summary>
     public partial class KoltsegvetesWindow : Window
     {
+
+        public void frissit()
+        {
+            
+        }
+
+        public void osszegez(List<PénzMozgás> Lista, Label lab)
+        {
+            int i = 0;
+            foreach (PénzMozgás p in Lista)
+            {
+                i += p.előirányzat;
+            }
+            lab.Content = i;
+        }
+
         public KoltsegvetesWindow(List<PénzMozgás> ILista, List<PénzMozgás> IILista, List<PénzMozgás> IIILista, List<PénzMozgás> IVLista, List<PénzMozgás> VLista, List<PénzMozgás> XIAaLista, List<PénzMozgás> XIAbLista, List<PénzMozgás> XIBaLista, List<PénzMozgás> XIBbLista, List<PénzMozgás> XIILista, List<PénzMozgás> XIIILista, List<PénzMozgás> XIVLista, List<PénzMozgás> XVLista, List<PénzMozgás> XVILista, List<PénzMozgás> XVIILista, List<PénzMozgás> XVIIILista)
         {
             InitializeComponent();
@@ -38,6 +54,38 @@ namespace Konyvelo
             XVIDataGrid.ItemsSource = XVILista;
             XVIIDataGrid.ItemsSource = XVIILista;
             XVIIIDataGrid.ItemsSource = XVIIILista;
+            osszegez(ILista, Iossz);
+            osszegez(IILista, IIossz);
+            osszegez(IIILista, IIIossz);
+            osszegez(IVLista, IVossz);
+            evesSajatBevetelNev.Content = "Az egyházközség " + System.DateTime.Now.Year + ". évi saját bevételei összesen:";
+            evesSajatBevetelErtek.Content = Convert.ToInt32(Iossz.Content) + Convert.ToInt32(IIossz.Content) + Convert.ToInt32(IIIossz.Content) + Convert.ToInt32(IVossz.Content);
+            osszegez(VLista, Vossz);
+            evesBevetelNev.Content = "Az egyházközség " + System.DateTime.Now.Year + ". évi bevételei összesen:";
+            EvesBevetelErtek.Content = Convert.ToInt32(evesSajatBevetelErtek.Content) + Convert.ToInt32(Vossz.Content);
+            osszegez(XIAaLista, XIAaossz);
+            osszegez(XIAbLista, XIAbossz);
+            XI_Aosszes.Content = Convert.ToInt32(XIAaossz.Content) + Convert.ToInt32(XIAbossz.Content);
+            osszegez(XIBaLista, XIBaossz);
+            osszegez(XIBbLista, XIBbossz);
+            XI_Bosszes.Content = Convert.ToInt32(XIBaossz.Content) + Convert.ToInt32(XIBbossz.Content);
+            XIosszes.Content = Convert.ToInt32(XI_Aosszes.Content) + Convert.ToInt32(XI_Bosszes.Content);
+            osszegez(XIILista, XIIossz);
+            osszegez(XIIILista, XIIIossz);
+            osszegez(XIVLista, XIVossz);
+            osszegez(XVLista, XVossz);
+            osszegez(XVILista, XVIossz);
+            osszegez(XVIILista, XVIIossz);
+            evesSajatKiadasNev.Content = "Az egyházközség " + System.DateTime.Now.Year + ". évi saját kiadásai összesen:";
+            evesSajatKiadasErtek.Content = Convert.ToInt32(XIosszes.Content) + Convert.ToInt32(XIIossz.Content) + Convert.ToInt32(XIIossz.Content) + Convert.ToInt32(XIVossz.Content) + Convert.ToInt32(XVossz.Content) + Convert.ToInt32(XVIossz.Content) + Convert.ToInt32(XVIossz.Content) + Convert.ToInt32(XVIIossz.Content);
+            evesKiadasNev.Content = "Az egyházközség " + System.DateTime.Now.Year + ". évi kiadásai összesen:";
+            osszegez(XVIIILista, XVIIIossz);
+            evesKiadasErtek.Content = Convert.ToInt32(evesSajatKiadasErtek.Content) + Convert.ToInt32(XVIIIossz.Content);
+            osszesEloiranyzottEvesBevetelNev.Content = "Összes előirányzott bevétel a " + System.DateTime.Now.Year + ". évre:";
+            osszesEloiranyzottEvesBevetelErtek.Content = EvesBevetelErtek.Content;
+            osszesEloiranyzottEvesKiadasNev.Content = "Összes előirányzott kiadás a " + System.DateTime.Now.Year + ". évre:";
+            osszesEloiranyzottEvesKiadasErtek.Content = evesKiadasErtek.Content;
+            kulonbozet.Content = Convert.ToInt32(osszesEloiranyzottEvesBevetelErtek.Content) - Convert.ToInt32(osszesEloiranyzottEvesKiadasErtek.Content);
             this.Left = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right - (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right / 2) - (this.Width / 2);
             this.Top = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Bottom - (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Bottom / 2) - (this.Height / 2);
         }
