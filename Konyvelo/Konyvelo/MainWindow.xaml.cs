ec0add4698b65cbe7a8eddc6a7e29d89a6f3095c
Alterion <hevnerjanos@hotmail.com>
2014-02-20 17:07:04 +0100

    just testing

--- Konyvelo/Konyvelo/MainWindow.xaml.cs
+++ Konyvelo/Konyvelo/MainWindow.xaml.cs
@@ -68,10 +68,13 @@ namespace Konyvelo
         }
         private List<Bejegyzés> LoadCollectionData()
         {
-            bejegyzések.Add(new Bejegyzés(1, "2014. jan. 1.", "Maradvány 2008-ról (házipénztár)", "IV/10", 4093002, 0, 0, 0, "BeB/2014"));
-            bejegyzések.Add(new Bejegyzés(2, "2014. jan. 1.", "Maradvány 2008-ról (bankszámla)", "IV/10", 0, 0, 110000, 0, "664-0184"));
-            bejegyzések.Add(new Bejegyzés(3, "2014. jan. 7.", "Postaköltség", "XVIII/2", 0, 0, 0, 5000, "KiP/2014"));
-            bejegyzések.Add(new Bejegyzés(4, "2014. jan. 8.", "Óévi perselyadomány", "XVII/1", 12000, 0, 0, 0, "KiB/2014"));
+            for (int i = 0; i < 100; i++)
+            {
+                bejegyzések.Add(new Bejegyzés(1, "2014. jan. 1.", "Maradvány 2008-ról (házipénztár)", "IV/10", 40, 0, 0, 0, "BeB/2014"));
+                bejegyzések.Add(new Bejegyzés(2, "2014. jan. 1.", "Maradvány 2008-ról (bankszámla)", "IV/10", 0, 0, 11, 0, "664-0184"));
+                bejegyzések.Add(new Bejegyzés(3, "2014. jan. 7.", "Postaköltség", "XVIII/2", 0, 0, 0, 50, "KiP/2014"));
+                bejegyzések.Add(new Bejegyzés(4, "2014. jan. 8.", "Óévi perselyadomány", "XVII/1", 12, 0, 0, 0, "KiB/2014"));
+            }
 
             return bejegyzések;
         }
@@ -377,6 +380,7 @@ namespace Konyvelo
             listazo();
             //MyDataGrid.ItemsSource = LoadCollectionData(); ///LoadCFromFile(PénzMozgás.évSzám.ToString());bejegyzések listát feltöltő metódus
             //SaveTo(PénzMozgás.évSzám.ToString());
+            LoadCollectionData();
             MyDataGrid.IsReadOnly = true;
             generateLists();//ha a pénzmozgás tábla üres akkor kell csak lefuttatni ezt a metódust és elmenteni a táblába, ha nem üres akkor a táblából kell betölteni az adatokat a listákba
             MyDataGrid.ItemsSource = bejegyzések;
